d47db29ac00f028af0334c06c191eb7c7a8dea55
AvikSharafat248 <aviklennon@gmail.com>
2024-09-23 02:27:09 +0600

    gave functioning to the first donation button

---
+++ index.js
@@ -0,0 +1,21 @@
+document.getElementById('first-donation-btn').addEventListener('click',
+    function(event){
+        event.preventDefault()
+
+        const input1 = document.getElementById('first-donation-input').value
+        const Amount = document.getElementById('first-donation-amount').innerText
+
+        const fullAmount = document.getElementById('total-amount').innerText
+
+        if(!isNaN(input1) && input1 >= 0){
+    const addedAmount = parseFloat(Amount) + parseFloat(input1)
+    const subtractAmount =parseFloat(fullAmount) - addedAmount 
+    document.getElementById('first-donation-amount').innerText = addedAmount
+
+    document.getElementById('total-amount').innerText = subtractAmount
+        }else{
+            alert('Please enter a valid number')
+        }
+        
+    }
+)
