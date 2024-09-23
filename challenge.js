7f681c30446c41b0319e7bc9d29db0f69d22a9eb
AvikSharafat248 <aviklennon@gmail.com>
2024-09-24 02:59:12 +0600

    added some part

--- challenge.js
+++ challenge.js
@@ -1,7 +1,11 @@
 document.getElementById('history-btn').addEventListener('click', function(){
     const don = document.getElementById('Donation5')
-    const history2 = document.getElementById('history-2')
+    const history1 = document.getElementById('hidden-1')
+    const history2 = document.getElementById('hidden-2')
+    const history4 = document.getElementById('hidden-3')
 
     don.classList.add('hidden')
+    history1.classList.remove('hidden')
     history2.classList.remove('hidden')
+    history4.classList.remove('hidden')
 })
--- challenge2.js
+++ challenge2.js
@@ -1,7 +1,12 @@
 document.getElementById('Donation-BTN').addEventListener('click', function(){
     const don2 = document.getElementById('Donation5')
-    const history3 = document.getElementById('history-2')
+    const history1 = document.getElementById('hidden-1')
+    const history2 = document.getElementById('hidden-2')
+    const history3 = document.getElementById('hidden-3')
+
 
     don2.classList.remove('hidden')
+    history1.classList.add('hidden')
+    history2.classList.add('hidden')
     history3.classList.add('hidden')
 })
