55117f234adbfed42c6ec932b97b28c0112ba9dc
AvikSharafat248 <aviklennon@gmail.com>
2024-09-24 01:28:31 +0600

    added some part

---
+++ challenge.js
@@ -0,0 +1,7 @@
+document.getElementById('history-btn').addEventListener('click', function(){
+    const don = document.getElementById('Donation5')
+    const history2 = document.getElementById('history-2')
+
+    don.classList.add('hidden')
+    history2.classList.remove('hidden')
+})
---
+++ challenge2.js
@@ -0,0 +1,7 @@
+document.getElementById('Donation-BTN').addEventListener('click', function(){
+    const don2 = document.getElementById('Donation5')
+    const history3 = document.getElementById('history-2')
+
+    don2.classList.remove('hidden')
+    history3.classList.add('hidden')
+})
